7c7b802424ddde6651076042a0d2d73ade6c5cb4
ismaelcmajada <ismaelcmajada@gmail.com>
2025-02-23 17:27:29 +0000

    Add PrimeNG and Tailwind CSS support, update app routing, and clean up initial template and styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from "@angular/core"
+import { RouterOutlet } from "@angular/router"
+import { ButtonModule } from "primeng/button"
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  selector: "app-root",
+  imports: [RouterOutlet, ButtonModule],
+  templateUrl: "./app.component.html",
+  styleUrl: "./app.component.css",
 })
 export class AppComponent {
-  title = 'secretos_angular';
+  title = "secretos_angular"
 }
---
+++ src/app/hub/hub.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-hub',
+  imports: [],
+  templateUrl: './hub.component.html',
+  styleUrl: './hub.component.css'
+})
+export class HubComponent {
+
+}
